8b3e254f314a2985cb2ae1a30f0ec58095bc3a0b
SpartanCheung <zd853768760@gmail.com>
2018-03-25 21:39:04 +0800

    Test Code for 4 wire touchscreen position monitor

---
+++ touchScreen/Debug/core/abi.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\SpartanCheung\AppData\Local\Temp\VMBuilds\touchScreen\uno\Debug\core\abi.cpp.o: \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\abi.cpp
---
+++ touchScreen/Debug/core/wiring_pulse.S.d
@@ -0,0 +1,2 @@
+C:\Users\SpartanCheung\AppData\Local\Temp\VMBuilds\touchScreen\uno\Debug\core\wiring_pulse.S.o: \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\wiring_pulse.S
